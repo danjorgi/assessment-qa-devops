5f82b796cdfaedfe5613d1f1f08dd7689d8c31e9
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-11-21 19:44:18 -0700

    Part 2 and 3 Complete

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,16 @@
 const shuffle = require("../src/shuffle");
 
 describe("shuffle should...", () => {
-  // CODE HERE
+  test('return an array', () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    const result = shuffle(inputArray);
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  test('return an array the same length as the argument sent in', () => {
+    const inputArray = ['apple', 'banana', 'orange', 'grape'];
+    const result = shuffle(inputArray);
+    expect(result).toHaveLength(inputArray.length);
+  })
+
 });
